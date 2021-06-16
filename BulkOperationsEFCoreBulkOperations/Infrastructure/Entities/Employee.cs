b8386e5a97873dc506470eaafc4386bbe8642967
mdrizy96 <duncanokeno@gmail.com>
2021-06-16 11:28:37 +0300

    Add packages

---
+++ BulkOperationsEFCoreBulkOperations/Infrastructure/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkOperationsEFCoreBulkOperations.Infrastructure.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string City { get; set; }
+    }
+}
